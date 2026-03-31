a30bd020d6ed0a389e777d870a749cf16ecc8589
HUGO DE OLIVEIRA ALVES VIEIRA <25203638@CFP127ALUNO.LOCAL>
2026-03-31 11:26:03 -0300

    atividade 7 e mandamento

--- 07-financiamento/modulo/script.js
+++ 07-financiamento/modulo/script.js
@@ -1,1 +1,40 @@
 'use stric'
+
+function criarListaNumeros(valor){
+    let listaNumero = []
+
+    for(let i = 1; i <= valor; i++){
+        listaNumero.push(i)
+    }
+    return listaNumero
+}
+
+function criarListaParcela (valorTotal, valorParcela){
+    let listaParcela = []
+    let result
+
+    for(let i = 1; i <= valorParcela; i++){
+
+        result = valorTotal / valorParcela
+        listaParcela.push(Number(result).toFixed(2))
+    }
+
+    return listaParcela
+}
+function jurosMes (valorTotal, valorTaxa, valorParcela){
+
+    let saldo = valorTotal
+    let taxa = valorTaxa
+    let parcelas = valorParcela
+
+    for (let i = 1; i <= parcelas; i++) {
+
+        saldo * (taxa / 100)
+
+    }
+}
+function totalMes (){
+
+}
+function saldoDevedor (){}
+
